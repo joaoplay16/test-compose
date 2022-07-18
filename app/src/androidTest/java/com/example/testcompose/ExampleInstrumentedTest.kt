4f69252d5948cc1e70ec068a442f4d7dc37637dd
João Pedro <joaoplay16@gmail.com>
2022-07-17 21:18:46 -0300

    tests added

--- app/src/androidTest/java/com/example/testcompose/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/testcompose/ExampleInstrumentedTest.kt
@@ -1,5 +1,22 @@
 package com.example.testcompose
 
+import android.graphics.Bitmap
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.unit.dp
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +24,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,4 +39,20 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.testcompose", appContext.packageName)
     }
+
+
+    @get:Rule
+    val rule = createComposeRule()
+
+    @Test
+    fun testAnimation() {
+        
+        rule.setContent { 
+            CompassAnimation()
+        }
+        rule.onNode(hasContentDescription("BoxContainer"))
+
+        rule.onRoot().printToLog("BoxContainer")
+    }
+
 }
