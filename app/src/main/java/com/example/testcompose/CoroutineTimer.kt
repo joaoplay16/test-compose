5483957c78aa344224e9811404750e1dc633f0da
João Pedro <joaoplay16@gmail.com>
2023-01-06 22:02:22 -0300

    create a countdown timer using coroutines

---
+++ app/src/main/java/com/example/testcompose/CoroutineTimer.kt
@@ -0,0 +1,36 @@
+package com.example.testcompose
+
+import kotlinx.coroutines.*
+
+class CoroutineTimer(
+    private val delayMillis: Long = 0,
+    val repeatMillis: Long = 0,
+    val action: () -> Unit
+) {
+
+    private val job = SupervisorJob()
+    private val scope = CoroutineScope(Dispatchers.Main + job)
+
+    private fun startCoroutineTimer() = scope.launch(Dispatchers.Main) {
+        delay(delayMillis)
+        if (repeatMillis > 0) {
+            while (true) {
+                action()
+                delay(repeatMillis)
+            }
+        } else {
+            action()
+        }
+    }
+
+    private val timer: Job by lazy{  startCoroutineTimer() }
+
+    fun startTimer() {
+        timer.start()
+    }
+
+    fun cancelTimer() {
+        timer.cancel()
+    }
+
+}
