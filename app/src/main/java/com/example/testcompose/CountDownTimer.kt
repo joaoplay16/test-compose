2aced7124c4b56e13df9e907c7be995810919da4
João Pedro <joaoplay16@gmail.com>
2023-01-08 22:45:15 -0300

    play sound in the composable preview

--- app/src/main/java/com/example/testcompose/CountDownTimer.kt
+++ app/src/main/java/com/example/testcompose/CountDownTimer.kt
@@ -9,9 +9,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.testcompose.ui.theme.TestComposeTheme
+import com.example.testcompose.util.playSound
 import kotlin.concurrent.timer
 
 
@@ -52,11 +54,13 @@ fun CountDownTimer(
 @Composable
 fun PreviewCountDownTimer() {
     TestComposeTheme() {
+        val context = LocalContext.current
         Surface {
             CountDownTimer(
                 modifier = Modifier.fillMaxSize(),
-                timeInMillis = 10_000L,
+                timeInMillis = 50_000L,
                 action = {
+                    playSound(context, R.raw.beep)
                     Log.d("Timer", "prod at problem")
                 }
             )
