d5df2fb31b8373aa7fa436bfcea561632602fc86
João Pedro <joaoplay16@gmail.com>
2023-01-10 19:55:03 -0300

    use time view-model implementation and launch countdown timer in a rememberCoroutineScope to call the same coroutine instance during recomposition

--- app/src/main/java/com/example/testcompose/CountDownTimer.kt
+++ app/src/main/java/com/example/testcompose/CountDownTimer.kt
@@ -14,13 +14,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.testcompose.ui.theme.TestComposeTheme
 import com.example.testcompose.util.playSound
+import com.example.testcompose.viewmodel.TimeViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlin.concurrent.timer
 
 
 @Composable
 fun CountDownTimer(
     modifier: Modifier = Modifier,
-    timeInMillis: Long ,
+    timeInMillis: Long,
     period: Long = 1_000L,
     action: () -> Unit = {}
 ) {
@@ -53,15 +56,24 @@ fun CountDownTimer(
 @Preview
 @Composable
 fun PreviewCountDownTimer() {
+    val coroutineScope = rememberCoroutineScope()
+
+    val viewModel = TimeViewModel()
+    viewModel.setTimeValue(50_000L)
+    val timeInMillis by viewModel.timer
+
     TestComposeTheme() {
         val context = LocalContext.current
         Surface {
             CountDownTimer(
                 modifier = Modifier.fillMaxSize(),
-                timeInMillis = 50_000L,
+                timeInMillis = timeInMillis,
                 action = {
-                    playSound(context, R.raw.beep)
-                    Log.d("Timer", "prod at problem")
+                    coroutineScope.launch(Dispatchers.IO) {
+                        playSound(context, R.raw.beep)
+                        viewModel.setTimeValue(timeInMillis - 1000)
+                        Log.d("Timer", "prod at problem ${timeInMillis / 1000}")
+                    }
                 }
             )
         }
