def5a887893d8a682d5bfb781893f335f82b389e
João Pedro <joaoplay16@gmail.com>
2023-01-18 22:18:26 -0300

    handle timer in view-model

--- app/src/main/java/com/example/testcompose/viewmodel/TimeViewModel.kt
+++ app/src/main/java/com/example/testcompose/viewmodel/TimeViewModel.kt
@@ -3,12 +3,37 @@ package com.example.testcompose.viewmodel
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import java.util.*
 
 class TimeViewModel : ViewModel() {
-    private var _timer: MutableState<Long> = mutableStateOf(0)
-    val timer = _timer
+    private var timer: Timer? = null
+
+    private var _timerValue: MutableState<Long> = mutableStateOf(0)
+
+    val timerValue = _timerValue
 
     fun setTimeValue(timeInMillis: Long){
-        _timer.value = timeInMillis
+        _timerValue.value = timeInMillis
+    }
+
+    fun startTimer(action: () -> Unit){
+        if (timerValue.value != 0L) {
+            timer = Timer()
+            viewModelScope.launch {
+                timer?.scheduleAtFixedRate( object : TimerTask() {
+                    override fun run() {
+                        _timerValue.value--
+                        action()
+                        if (timerValue.value == 0L) this.cancel()
+                    }
+                }, 0L, 1000L)
+            }
+        }
+    }
+
+    fun stopTimer(){
+        timer?.cancel()
     }
 }
