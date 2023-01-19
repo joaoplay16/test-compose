c5d1e1d45d42037db9be39755989738efca092b4
João Pedro <joaoplay16@gmail.com>
2023-01-18 22:22:34 -0300

    set up countdown timer

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.testcompose
 
+import android.content.Context
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
@@ -19,33 +22,68 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.testcompose.ui.theme.TestComposeTheme
+import com.example.testcompose.util.playSound
+import com.example.testcompose.viewmodel.TimeViewModel
 
 class MainActivity : ComponentActivity() {
+    val timerViewModel by viewModels<TimeViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             TestComposeTheme {
-                MyApp()
+                MyApp(this, timerViewModel)
             }
         }
     }
 }
 
 @Composable
-fun MyApp() {
+fun MyApp( context: Context, timerViewModel: TimeViewModel,) {
 
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
         OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     } else {
-        Greetings()
+
+        val count by timerViewModel.timerValue
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+            TextField(
+                value = count.toString(),
+                modifier = Modifier.background(Color.DarkGray),
+                placeholder = { Text(text = "Digite")},
+                onValueChange = {
+                    timerViewModel.setTimeValue(it.ifEmpty { "0" }.toLong())
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword)
+            )
+
+            CountDownTimer(
+                modifier = Modifier.fillMaxSize(),
+                count = count.toString(),
+                start = {
+                    timerViewModel.startTimer(action = {
+                        playSound(context, R.raw.beep)
+                    })
+                },
+                stop = {
+                    timerViewModel.stopTimer()
+                },
+            )
+        }
     }
+
 }
 
 @Composable
@@ -115,9 +153,9 @@ fun CardContent(name: String) {
             )
             if (expanded){
                 Text(
-                   text = ("Composem ipsum color sit lazy, " +
+                    text = ("Composem ipsum color sit lazy, " +
                             "padding theme elit, sed do bouncy. ").repeat(4),
-                    )
+                )
             }
         }
 
