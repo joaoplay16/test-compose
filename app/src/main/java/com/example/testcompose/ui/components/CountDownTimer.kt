9624b06c711fd332004542ad56aeebd8229ad960
João Pedro <joaoplay16@gmail.com>
2023-01-18 22:21:49 -0300

    turn into a pure component

--- app/src/main/java/com/example/testcompose/ui/components/CountDownTimer.kt
+++ app/src/main/java/com/example/testcompose/ui/components/CountDownTimer.kt
@@ -1,12 +1,11 @@
 package com.example.testcompose
 
-import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -15,66 +14,61 @@ import androidx.compose.ui.unit.sp
 import com.example.testcompose.ui.theme.TestComposeTheme
 import com.example.testcompose.util.playSound
 import com.example.testcompose.viewmodel.TimeViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlin.concurrent.timer
 
 
 @Composable
 fun CountDownTimer(
     modifier: Modifier = Modifier,
-    timeInMillis: Long,
-    period: Long = 1_000L,
-    action: () -> Unit = {}
+    count: String = "0",
+    start: () -> Unit,
+    stop: () -> Unit,
 ) {
-
-    var count by remember{ mutableStateOf(timeInMillis / period ) }
-
-    LaunchedEffect(key1 = null, block = {
-        timer("timer", false, 0, period) {
-            count--
-
-            if (count <= 0) this.cancel()
-
-            action()
-
-        }
-    })
 
     Column(
         modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-        ) {
-        Text(
-            text = "${count}",
-            fontSize = 34.sp
-        )
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = count, fontSize = 48.sp)
+
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround){
+            Button(onClick = {
+                start()
+            }) {
+                Text(text = "Start", fontSize = 48.sp)
+            }
+            Button(onClick = {
+                stop()
+            }) {
+                Text(text = "Stop", fontSize = 48.sp)
+            }
+        }
     }
 }
 
 @Preview
 @Composable
 fun PreviewCountDownTimer() {
-    val coroutineScope = rememberCoroutineScope()
-
     val viewModel = TimeViewModel()
-    viewModel.setTimeValue(50_000L)
-    val timeInMillis by viewModel.timer
+    viewModel.setTimeValue(30)
+    val count by viewModel.timerValue
 
     TestComposeTheme() {
         val context = LocalContext.current
         Surface {
             CountDownTimer(
                 modifier = Modifier.fillMaxSize(),
-                timeInMillis = timeInMillis,
-                action = {
-                    coroutineScope.launch(Dispatchers.IO) {
+                count = count.toString(),
+                start = {
+                    viewModel.startTimer(action = {
                         playSound(context, R.raw.beep)
-                        viewModel.setTimeValue(timeInMillis - 1000)
-                        Log.d("Timer", "prod at problem ${timeInMillis / 1000}")
-                    }
-                }
+                    })
+                },
+                stop = {
+                    viewModel.stopTimer()
+                },
             )
         }
     }
