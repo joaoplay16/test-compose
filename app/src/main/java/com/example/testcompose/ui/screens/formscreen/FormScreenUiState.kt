1094d44bf5f485ee919cd299db1bd8e91e3e0312
João Pedro <joaoplay16@gmail.com>
2023-05-07 23:43:11 -0300

    refact: use state instead of string

--- app/src/main/java/com/example/testcompose/ui/screens/formscreen/FormScreenUiState.kt
+++ app/src/main/java/com/example/testcompose/ui/screens/formscreen/FormScreenUiState.kt
@@ -1,6 +1,6 @@
 package com.example.testcompose.ui.screens.formscreen
 
 data class FormScreenUiState (
-    val email: String = "",
-    val password: String = ""
+    val email: EmailState = EmailState(),
+    val password: PasswordState = PasswordState()
 )
