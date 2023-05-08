e035b97166e9c0227d050b222cfc64e14dde525b
João Pedro <joaoplay16@gmail.com>
2023-05-08 20:32:09 -0300

    remove unused events

--- app/src/main/java/com/example/testcompose/ui/screens/formscreen/FormScreenUiEvent.kt
+++ app/src/main/java/com/example/testcompose/ui/screens/formscreen/FormScreenUiEvent.kt
@@ -1,7 +1,5 @@
 package com.example.testcompose.ui.screens.formscreen
 
 sealed class FormScreenUiEvent {
-    data class onEmailChanged(val email: EmailState) : FormScreenUiEvent()
-    data class onPasswordChanged(val password: PasswordState) : FormScreenUiEvent()
     object Submit: FormScreenUiEvent()
 }
