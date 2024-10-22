d8f7991858d48a94e9c60d9fe5f4f71e571f9799
João Pedro <joaoplay16@gmail.com>
2024-10-21 22:56:05 -0300

    Update the pull to refresh implementation

--- app/src/main/java/com/example/testcompose/ui/components/PullToRefreshLazyColumn.kt
+++ app/src/main/java/com/example/testcompose/ui/components/PullToRefreshLazyColumn.kt
@@ -11,13 +11,13 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
+import androidx.compose.material3.pulltorefresh.pullToRefresh
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -31,8 +31,18 @@ fun <T> PullToRefreshLazyColumn(
 ) {
     val pullToRefreshState = rememberPullToRefreshState()
     Box(
-        modifier = modifier.nestedScroll(pullToRefreshState.nestedScrollConnection)
+        modifier = modifier.pullToRefresh(
+            isRefreshing = isRefreshing,
+            onRefresh = onRefresh,
+            state = pullToRefreshState
+        )
     ) {
+        Indicator(
+            modifier = Modifier.align(Alignment.TopCenter),
+            isRefreshing = isRefreshing,
+            state = pullToRefreshState
+        )
+
         LazyColumn(
             state = lazyListState,
             contentPadding = PaddingValues(8.dp),
@@ -44,23 +54,10 @@ fun <T> PullToRefreshLazyColumn(
             }
         }
 
-        if (pullToRefreshState.isRefreshing) {
+        if (isRefreshing) {
             LaunchedEffect(true) {
                 onRefresh()
             }
         }
-
-        LaunchedEffect(isRefreshing) {
-            if (isRefreshing) {
-                pullToRefreshState.startRefresh()
-            } else {
-                pullToRefreshState.endRefresh()
-            }
-        }
-
-        PullToRefreshContainer(
-            state = pullToRefreshState,
-            modifier = Modifier.align(Alignment.TopCenter),
-        )
     }
 }
