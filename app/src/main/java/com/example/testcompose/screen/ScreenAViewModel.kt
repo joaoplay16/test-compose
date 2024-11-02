6564475211472c1f42d2226fb87500fa7dde5313
João Pedro <joaoplay16@gmail.com>
2024-11-01 21:57:46 -0300

    Implement SnackBar in any place

---
+++ app/src/main/java/com/example/testcompose/screen/ScreenAViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.testcompose.screen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.testcompose.screen.SnackbarAction
+import com.example.testcompose.screen.SnackbarController
+import com.example.testcompose.screen.SnackbarEvent
+import kotlinx.coroutines.launch
+
+class ScreenAViewModel: ViewModel() {
+
+    fun showSnackbar() {
+        viewModelScope.launch {
+            SnackbarController.sendEvent(
+                event = SnackbarEvent(
+                    message = "Hello from ViewModel",
+                    action = SnackbarAction(
+                        name = "Click me!",
+                        action = {
+                            SnackbarController.sendEvent(
+                                event = SnackbarEvent(
+                                    message = "Action pressed!"
+                                )
+                            )
+                        }
+                    )
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/testcompose/screen/ScreenC.kt
@@ -0,0 +1,50 @@
+package com.example.testcompose.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+@Composable
+fun ScreenC(
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: ScreenAViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
+) {
+    val scope = rememberCoroutineScope()
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = {
+            scope.launch {
+                SnackbarController.sendEvent(
+                    event = SnackbarEvent(
+                        message = "Hello world!"
+                    )
+                )
+            }
+        }) {
+            Text(text = "Show snackbar")
+        }
+        Button(onClick = {
+            viewModel.showSnackbar()
+        }) {
+            Text(text = "Show snackbar with action")
+        }
+        Button(onClick = onNavigate) {
+            Text(text = "Navigate to D")
+        }
+    }
+}
