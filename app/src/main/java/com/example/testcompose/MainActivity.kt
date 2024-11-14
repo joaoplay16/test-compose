50b5f887d2b1685ce141a62ce8abd3a1248a431e
João Pedro <joaoplay16@gmail.com>
2024-11-13 21:44:43 -0300

    Try SubcomposeAsyncImage

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -9,25 +9,46 @@ import androidx.activity.viewModels
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.SubcomposeAsyncImage
 import com.example.testcompose.ui.theme.TestComposeTheme
 import com.example.testcompose.util.playSound
 import com.example.testcompose.viewmodel.TimerViewModel
@@ -40,14 +61,20 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             TestComposeTheme {
-                MyApp(this, timerViewModel)
+                MyApp(
+                    this,
+                    timerViewModel
+                )
             }
         }
     }
 }
 
 @Composable
-fun MyApp(context: Context, timerViewModel: TimerViewModel,) {
+fun MyApp(
+    context: Context,
+    timerViewModel: TimerViewModel,
+) {
 
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
@@ -62,7 +89,7 @@ fun MyApp(context: Context, timerViewModel: TimerViewModel,) {
             TextField(
                 value = count.toString(),
                 modifier = Modifier.background(Color.DarkGray),
-                placeholder = { Text(text = "Digite")},
+                placeholder = { Text(text = "Digite") },
                 onValueChange = {
                     timerViewModel.setTimeValue(it.ifEmpty { "0" }.toLong())
                 },
@@ -74,7 +101,10 @@ fun MyApp(context: Context, timerViewModel: TimerViewModel,) {
                 count = count.toString(),
                 start = {
                     timerViewModel.startTimer(action = {
-                        playSound(context, R.raw.beep)
+                        playSound(
+                            context,
+                            R.raw.beep
+                        )
                     })
                 },
                 stop = {
@@ -107,7 +137,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 }
 
 @Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         items(items = names) { name ->
             Greeting(name = name)
@@ -125,7 +155,10 @@ private fun Greeting(name: String) {
             disabledContainerColor = Color.Unspecified,
             disabledContentColor = Color.Unspecified,
         ),
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        modifier = Modifier.padding(
+            vertical = 4.dp,
+            horizontal = 8.dp
+        )
     ) {
         CardContent(name)
     }
@@ -133,7 +166,7 @@ private fun Greeting(name: String) {
 
 @Composable
 fun CardContent(name: String) {
-    var expanded by remember { mutableStateOf(false)}
+    var expanded by remember { mutableStateOf(false) }
 
     Row(
         modifier = Modifier
@@ -144,10 +177,11 @@ fun CardContent(name: String) {
                     stiffness = Spring.StiffnessLow
                 )
             )
-    ){
-        Column(modifier = Modifier
-            .weight(1f)
-            .padding(12.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
         ) {
             Text(text = "Hello, ")
             Text(
@@ -156,7 +190,7 @@ fun CardContent(name: String) {
                     fontWeight = FontWeight.ExtraBold
                 )
             )
-            if (expanded){
+            if (expanded) {
                 Text(
                     text = ("Composem ipsum color sit lazy, " +
                             "padding theme elit, sed do bouncy. ").repeat(4),
@@ -186,7 +220,10 @@ fun CardContent(name: String) {
     name = "DefaultPreviewDark"
 )
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320
+)
 @Composable
 fun DefaultPreview() {
     TestComposeTheme {
@@ -194,10 +231,45 @@ fun DefaultPreview() {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320
+)
 @Composable
 fun OnboardingPreview() {
     TestComposeTheme {
         OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Preview()
+@Composable
+fun AsyncImagePreview() {
+    TestComposeTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(32.dp)
+            ) {
+                SubcomposeAsyncImage(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(16f / 9),
+                    model = "https://jooinn.com/images/dog-67.jpg",
+                    contentDescription = null,
+                    error = {
+                        Image(
+                            painter = painterResource(id = R.drawable.baseline_baby_changing_station_24),
+                            contentDescription
+                        )
+                    }
+                )
+                Text(text = "Image")
+            }
+        }
     }
 }
