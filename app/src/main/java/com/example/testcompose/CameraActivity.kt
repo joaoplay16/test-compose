79c0045b02c58572b9e509fa16a994b738b9acda
João Pedro <joaoplay16@gmail.com>
2025-01-18 22:31:40 -0300

    Replace LifecycleOwner deprecated import
    'LocalLifecycleOwner: ProvidableCompositionLocal<LifecycleOwner>' is deprecated. Moved to lifecycle-runtime-compose library in androidx. lifecycle. compose package.

--- app/src/main/java/com/example/testcompose/CameraActivity.kt
+++ app/src/main/java/com/example/testcompose/CameraActivity.kt
@@ -41,11 +41,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import coil3.compose.rememberAsyncImagePainter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
