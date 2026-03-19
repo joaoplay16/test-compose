22dc8bb70c9ccdb1d488e8416e31aafe7722738e
João Pedro <joaoplay16@gmail.com>
2026-03-19 11:10:50 -0300

    Show weight text in main activity

--- app/src/main/java/com/example/testcompose/WeightPickerActivity.kt
+++ app/src/main/java/com/example/testcompose/WeightPickerActivity.kt
@@ -7,9 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.testcompose.presentation.model.Scale
 import com.example.testcompose.screen.ScaleStyle
 import com.example.testcompose.ui.theme.TestComposeTheme
@@ -19,17 +28,26 @@ class WeightPickerActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var weight by remember {
+                mutableIntStateOf(value = 80)
+            }
+
             TestComposeTheme {
                 Box(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
                 ){
+
+                    Text(weight.toString(), fontSize = 36.sp)
+
                     Scale(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .align(Alignment.Center),
+                            .height(300.dp)
+                            .align(Alignment.BottomCenter),
                         style = ScaleStyle( scaleWidth =  150.dp)
                     ){
-
+                       weight = it
                     }
                 }
             }
