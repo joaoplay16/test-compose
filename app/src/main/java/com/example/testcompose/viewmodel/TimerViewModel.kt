780d69640339b3872f96bbeb7df9f62014ae1034
João Pedro <joaoplay16@gmail.com>
2026-03-22 22:18:32 -0300

    Replace obsolete `scheduleAtFixedRate`with `schedule`

--- app/src/main/java/com/example/testcompose/viewmodel/TimerViewModel.kt
+++ app/src/main/java/com/example/testcompose/viewmodel/TimerViewModel.kt
@@ -22,7 +22,7 @@ class TimerViewModel : ViewModel() {
         if (timerValue.value != 0L && timer == null) {
             timer = Timer()
             viewModelScope.launch {
-                timer?.scheduleAtFixedRate( object : TimerTask() {
+                timer?.schedule( object : TimerTask() {
                     override fun run() {
                         _timerValue.value--
                         action()
