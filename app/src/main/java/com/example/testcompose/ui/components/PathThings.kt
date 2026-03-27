24c8f7ff9ef10bfb786f007d9d4b531bcdc0fcef
João Pedro <joaoplay16@gmail.com>
2026-03-27 20:56:34 -0300

    Add arrow head to the path animation

--- app/src/main/java/com/example/testcompose/ui/components/PathThings.kt
+++ app/src/main/java/com/example/testcompose/ui/components/PathThings.kt
@@ -13,13 +13,17 @@ import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.PathMeasure
 import androidx.compose.ui.graphics.PathOperation
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.asAndroidPath
+import androidx.compose.ui.graphics.asComposePath
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlin.math.PI
+import kotlin.math.atan2
 
 @Composable
 fun DrawPath(modifier: Modifier = Modifier) {
@@ -73,7 +77,8 @@ fun DrawPathAnimation(modifier: Modifier = Modifier) {
     }
     LaunchedEffect(key1 = true) {
         pathPortion.animateTo(
-            targetValue = 1f, animationSpec = tween(
+            targetValue = 1f,
+            animationSpec = tween(
                 durationMillis = 2500
             )
         )
@@ -82,23 +87,36 @@ fun DrawPathAnimation(modifier: Modifier = Modifier) {
         moveTo(100f, 100f)
         quadraticTo(400f, 400f, 100f, 400f)
     }
-    val outPath = Path()
-    PathMeasure().apply {
-        setPath(path, false)
-        getSegment(
-            0f,
-            pathPortion.value * length,
-            outPath,
-            true
-        )
+    val outPath = android.graphics.Path()
+    val pos = FloatArray(2)
+    val tan = FloatArray(2)
+    android.graphics.PathMeasure().apply {
+        setPath(path.asAndroidPath(), false)
+        getSegment(0f, pathPortion.value * length, outPath, true)
+        getPosTan(pathPortion.value * length, pos, tan)
     }
 
     Canvas(modifier = modifier.fillMaxSize()) {
         drawPath(
-            path = outPath,
+            path = outPath.asComposePath(),
             color = Color.Red,
             style = Stroke(width = 5.dp.toPx(), cap = StrokeCap.Round)
         )
+
+        val x = pos[0]
+        val y = pos[1]
+        val degrees = -atan2(tan[0], tan[1]) * (180f / PI.toFloat()) - 180f
+        rotate(degrees = degrees, pivot = Offset(x, y)) {
+            drawPath(
+                path = Path().apply {
+                    moveTo(x, y - 30f)
+                    lineTo(x - 30f, y + 60f)
+                    lineTo(x + 30f, y + 60f)
+                    close()
+                },
+                color = Color.Red
+            )
+        }
     }
 }
 
