4b57ddfa829ff501cd1922e158630a4813a05831
João Pedro <joaoplay16@gmail.com>
2026-03-28 22:03:11 -0300

    Create GenderPickerActivity

---
+++ app/src/main/java/com/example/testcompose/GenderPickerActivity.kt
@@ -0,0 +1,22 @@
+package com.example.testcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.example.testcompose.ui.components.GenderPicker
+import com.example.testcompose.ui.theme.TestComposeTheme
+
+class GenderPickerActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TestComposeTheme {
+
+            }
+        }
+    }
+}
